2ac9512fe9f2c79fd0e316d6d2366a3b2681380a
doeun <rlaehdms28@naver.com>
2025-11-19 14:45:06 +0900

    20251119

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    String author;
+    String title;
+    String bookId;
+
+    public Book(String author, String title, String bookId) {
+        this.author = author;
+        this.title = title;
+        this.bookId = bookId;
+    }
+}
