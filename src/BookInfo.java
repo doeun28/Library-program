d2bc1d29057b7d611dccef6f1f6ee8776ef7fe77
younseo <part44480@gmail.com>
2025-11-20 11:50:03 +0900

    책 추가 기능(2025/11/20 목)-2024011913 박윤서

--- src/BookInfo.java
+++ src/BookInfo.java
@@ -1,17 +1,29 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class BookInfo {
-    private List<Book> bookList;
 
-    public BookInfo() {
-        this.bookList = new ArrayList<>();
-    }
+    private ArrayList<Book> books = new ArrayList<>();
+
+    // Client가 호출하는 메서드
     public boolean addBook(String author, String title, String bookId) {
-        return saveBook(author, title, bookId);
+        Book newBook = saveBook(author, title, bookId);
+        if (newBook != null) {
+            books.add(newBook);
+            return true;
+        }
+        return false;
     }
-    public boolean saveBook(String author, String title, String bookId) {
-        Book book = new Book(author, title, bookId);
-        return bookList.add(book);
+
+    // Book 객체 생성 기능
+    private Book saveBook(String author, String title, String bookId) {
+        return new Book(author, title, bookId);
+    }
+
+    // 전체 책 출력 (검색이 없으므로 참고용)
+    public void printBooks() {
+        for (Book b : books) {
+            System.out.println(b);
+        }
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        BookInfo info = new BookInfo();
 
+        // addBook 호출
+
+        System.out.println("[신규 도서 추가]");
+        System.out.println("--------------------------");
+        System.out.printf("1. 저자 입력 : ");
+        String author = sc.nextLine();
+        System.out.printf("2. 제목 입력 : ");
+        String title = sc.nextLine();
+        System.out.printf("3. 청구기호 입력 :");
+        String bookId = sc.nextLine();
+        System.out.println("--------------------------");
+        boolean result = info.addBook(author, title, bookId);
+        System.out.println("책 추가 결과: " + result);
+
+        // 현재까지 저장된 책 출력
+        info.printBooks();
     }
 }
+
