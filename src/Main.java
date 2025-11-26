7fff6c5806f0b721fe299217792e8579cc1f498c
younseo <part44480@gmail.com>
2025-11-26 15:47:04 +0900

    main.java 바꿈/ test 이름 바꿈

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,63 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        BookInfo manager = new BookInfo();
+        while (true) {
+            System.out.println("\n==================================");
+            System.out.println("[ 도서 관리 프로그램 v 1.0]");
+            System.out.println("==================================");
+            System.out.println("1. 도서 추가");
+            System.out.println("2. 도서 검색");
+            System.out.println("3. 종료");
+            System.out.println("==================================");
+            System.out.print("-> 메뉴를 선택하세요 : ");
+            String menu = sc.nextLine();
+            if (menu.equals("0")) {
+                System.out.println("-> 프로그램을 종료합니다.");
+                break;
+            }
+            else if (menu.equals("1")) {
+                System.out.println("\n[ 신규 도서 추가 ]");
+                System.out.println("---------------------------------------------");
 
-        BookInfo info = new BookInfo();
+                System.out.print("1. 저자 입력 : ");
+                String author = sc.nextLine();
 
-        // addBook 호출
+                System.out.print("2. 제목 입력 : ");
+                String title = sc.nextLine();
 
-        System.out.println("[신규 도서 추가]");
-        System.out.println("--------------------------");
-        System.out.printf("1. 저자 입력 : ");
-        String author = sc.nextLine();
-        System.out.printf("2. 제목 입력 : ");
-        String title = sc.nextLine();
-        System.out.printf("3. 청구기호 입력 :");
-        String bookId = sc.nextLine();
-        System.out.println("--------------------------");
-        boolean result = info.addBook(author, title, bookId);
-        System.out.println("책 추가 결과: " + result);
+                System.out.print("3. 청구기호 입력 : ");
+                String bookId = sc.nextLine();
+                System.out.println("---------------------------------------------");
 
-        // 현재까지 저장된 책 출력
-        info.printBooks();
+                boolean result = manager.addBook(author, title, bookId);
+
+                if (result) {
+                    System.out.println("-> 도서가 정상적으로 등록되었습니다!");
+                    System.out.println("   (제목: " + title + ", 저자: " + author + ")");
+                }
+            }
+
+//            else if (menu.equals("2")) {
+//                BookSearch search = new BookSearch(manager);
+//                System.out.println("\n[ 도서 검색 ]");
+//                System.out.println("---------------------------------------------");
+//                System.out.print("검색어 입력 (제목/저자/청구기호) : ");
+//                String keyword = sc.nextLine();
+//                System.out.println("---------------------------------------------");
+//
+//                List<Book> foundBook = search.searchBook(keyword);
+//
+//                if (foundBook != null) {
+//                    System.out.println("-> 검색 결과:");
+//                    System.out.println("   " + foundBook.toString());
+//                } else {
+//                    System.out.println("-> 찾는 도서가 없습니다.");
+//                }
+//            }
+        }
     }
 }
-
--- test/BookInfoTest.java
+++ test/BookInfoTest.java
@@ -6,9 +6,9 @@ public class BookInfoTest {
     void testAddBookPrintTrueFalse() {
         BookInfo info = new BookInfo();
 
-        boolean r1 = info.addBook("홍길동", "책1", "ID001");
-        boolean r2 = info.addBook("","책2", "ID002");
-        boolean r3 = info.addBook(null,"책2", "ID002");
+        boolean r1 = info.addBook("김철수", "파이썬입문", "808.8 김 64ㅇ");
+        boolean r2 = info.addBook("","파이썬입문", "808.8 김 64ㅇ");
+        boolean r3 = info.addBook(null,"파이썬입문", "808.8 김 64ㅇ");
 
         System.out.println("Test 1: " + r1);
         System.out.println("Test 2: " + r2);
