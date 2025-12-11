0a5b1b4c35157da6507d19191043347b9ad94e8b
doeun <rlaehdms28@naver.com>
2025-12-11 10:18:10 +0900

    a

--- src/Book.java
+++ src/Book.java
@@ -2,11 +2,13 @@ public class Book {
     private String author;
     private String title;
     private String bookId;
+    private String publisher;
 
-    public Book(String author, String title, String bookId) {
+    public Book(String author, String title, String bookId, String publisher) {
         this.author = author;
         this.title = title;
         this.bookId = bookId;
+        this.publisher = publisher;
     }
 
     public String getAuthor() {
@@ -18,9 +20,13 @@ public class Book {
     public String getBookId() {
         return bookId;
     }
+    public String getpublisher() {
+        return publisher;
+    }
+
 
     @Override
     public String toString() {
-        return "[" + "저자: " + author + ", 제목: " + title + ", 청구기호: " + bookId + "]";
+        return "[" + "저자: " + author + ", 제목: " + title + ", 청구기호: " + bookId + ", 출판사: " + "]";
     }
 }
